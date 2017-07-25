9c03e23ce8113ac76fe7c4b6afdedb8122295a60
Matteo Fratesi <matteo.fratesi@openmindonline.it>
2017-07-25 18:51:26 +0200

    assets organization

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,20 @@ var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('assets/stylesheets/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('assets/stylesheets/css/'))
 });
 
 gulp.task('minify-css', function() {
-  gulp.src('css/*.css')
+  gulp.src('assets/stylesheets/css/*.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('assets/stylesheets/build/'));
 });
 
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
-    gulp.watch('css/*.css',['minify-css']);
+    gulp.watch('assets/stylesheets/sass/**/*.scss',['styles']);
+    gulp.watch('assets/stylesheets/css/*.css',['minify-css']);
 });
